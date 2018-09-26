828ad2789377427ec7dda8e8aaaf2cb3e0300d1c
MeriChon <meri@fina2.net>
2018-09-26 16:35:08 +0400

    first commit

---
+++ Homework1/Assets/_Scripts/YellowBalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBalController : MonoBehaviour
+{
+    private void Awake()
+    {
+        //გამოიძახება მხოლოდ ერთხელ, სცენის ჩატვირთვამდე
+    }
+
+    private void Start()
+    {
+        //გამოიძახება მხოლოდ ერთხელ, სცენის ჩატვირთვის შემდეგ
+    }
+
+    private void Update()
+    {
+        //გამოიძახება ყოველ ფრეიმზე
+    }
+
+    private void OnEnable()
+    {
+        //გამოიძახება ყოველ ჯერზე როდესაც ობიექტი ხდება აქტიური (ინსპექტორში enable ღილაკის მონიშვნით)
+    }
+
+    private void OnDisable()
+    {
+        //გამოიძახება ყოველ ჯერზე როდესაც ობიექტი ხდება არა აქტიური (ინსპექტორში enable ღილაკის მონიშვნით)
+    }
+}
