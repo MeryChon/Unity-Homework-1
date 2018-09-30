6988463aa2a34acb00a4c71de0ed967cbb76f6ab
MeriChon <meri@fina2.net>
2018-09-30 14:55:15 +0400

    Removed lerp stuff

--- Homework1/Assets/_Scripts/YellowBalController.cs
+++ Homework1/Assets/_Scripts/YellowBalController.cs
@@ -7,28 +7,18 @@ public class YellowBalController : MonoBehaviour
 
     public Rigidbody _rigidBody;
 
-    private float x, y, z, startTime, speed, totalDistance;
-    private Vector3 initialPosition, startPosition, endPosition;
+    private Vector3 initialPosition;
 
 
     private void Awake()
     {
         //გამოიძახება მხოლოდ ერთხელ, სცენის ჩატვირთვამდე
-        this.x = 4f;
-        this.y = 4f;
-        this.z = 0f;
-        this.initialPosition = new Vector3(this.x, this.y, this.z);
+        this.initialPosition = new Vector3(4f, 4f, 0f);
     }
 
     private void Start()
     {
         //გამოიძახება მხოლოდ ერთხელ, სცენის ჩატვირთვის შემდეგ
-        this.startTime = Time.time;
-        this.speed = 20f;
-        this.totalDistance = 4f;
-        
-        this.startPosition = new Vector3(this.x, 0, this.z);
-        this.endPosition = new Vector3(this.x, this.y, this.z);
         
     }
 
@@ -36,27 +26,13 @@ public class YellowBalController : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0) || transform.position == this.initialPosition)
         {
-            Debug.Log("before setting " + this._rigidBody.velocity);
             this._rigidBody.velocity = new Vector3(0, 0, 0);
-            Debug.Log("after setting " + this._rigidBody.velocity);
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            this.startPosition = transform.position;
-            this.startTime = Time.time;
-            
         }
 
         //გამოიძახება ყოველ ფრეიმზე
         if (Input.GetMouseButton(0))  //მარცხენა  ღილაკზე დაჭერისას უბრუნდება საწყის პოზიციას და რჩება იქ, სანამ ღილაკს არ ავუშვებთ ხელს
         {
-            //Debug.Log(this.startPosition);
-            float distanceCovered = (Time.time - this.startTime) * this.speed;
-            //Debug.Log(distanceCovered);
-            float coveredFraction = distanceCovered / this.totalDistance;
-            transform.position = Vector3.Lerp(this.startPosition, this.endPosition, coveredFraction);
-            //transform.position = new Vector3(this.x, this.y, this.z);
+            transform.position = initialPosition;
         }
 
 
@@ -72,6 +48,7 @@ public class YellowBalController : MonoBehaviour
     private void OnDisable()
     {
         //გამოიძახება ყოველ ჯერზე როდესაც ობიექტი ხდება არა აქტიური (ინსპექტორში enable ღილაკის მონიშვნით)
+        this._rigidBody.velocity = new Vector3(0, 0, 0);
     }
 
 }
