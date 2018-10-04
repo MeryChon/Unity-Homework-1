2ce12844f36747a07d3d4718c2c68d8efdf6d795
MeriChon <meri@fina2.net>
2018-10-04 18:45:21 +0400

    started working on hw2

---
+++ Homework2/Assets/_Scripts/BallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    private Rigidbody _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        this._rigidBody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
