5ed3433a42d42353e02f8d161e9e68094a978e45
MeriChon <meri@fina2.net>
2018-10-29 18:13:15 +0400

    started working on homework 4

---
+++ Homework4/Assets/Scripts/CorgiController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorgiController : MonoBehaviour {
+
+    [SerializeField]
+    private float _velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 oldPosition = transform.position;
+        Vector2 shift; //= new Vector2(0, 0);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = oldPosition + new Vector2(-_velocity, 0);
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = oldPosition + new Vector2(_velocity, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            //TODO: jump
+        }
+
+    }
+}
