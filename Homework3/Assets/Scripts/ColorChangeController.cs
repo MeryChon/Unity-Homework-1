a094772af13eb1817f3315f714df967bf4af3590
MeriChon <meri@fina2.net>
2018-12-30 17:24:43 +0400

    Implemented color changing sliders

---
+++ Homework3/Assets/Scripts/ColorChangeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChangeController : MonoBehaviour
+{
+    [SerializeField]
+    private Color _color;
+    [SerializeField]
+    private Image _backgroundImage;
+
+    private Color _initialColor;
+    private float _previousSliderValue;
+
+    public void ChangeCanvasBackground(float colorSliderValue)
+    {
+        _initialColor = _backgroundImage.color;
+        Color targetColor = _initialColor + _color * (colorSliderValue - _previousSliderValue);
+        _backgroundImage.color = Color.Lerp(_initialColor, targetColor, 0.5f);
+        _previousSliderValue = colorSliderValue;
+    }
+}
--- Homework3/Assets/Scripts/HealthBarController.cs
+++ Homework3/Assets/Scripts/HealthBarController.cs
@@ -3,23 +3,8 @@ using UnityEngine.UI;
 
 public class HealthBarController : MonoBehaviour {
 
-    [SerializeField]
-    private Slider slider;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        
-	}
-
     public void ChangeBarWidth(float value)
     {
-        Debug.Log(value);
         var bar = GetComponent<Scrollbar>();
         bar.value = value;
     }
