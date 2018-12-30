b1bbd9822478b7780e486004eae9730667c59e63
MeriChon <meri@fina2.net>
2018-12-30 10:38:49 +0400

    Homework3 health slider working

---
+++ Homework3/Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+
+    [SerializeField]
+    private Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+
+    public void ChangeBarWidth(float value)
+    {
+        Debug.Log(value);
+        var bar = GetComponent<Scrollbar>();
+        bar.value = value;
+    }
+}
