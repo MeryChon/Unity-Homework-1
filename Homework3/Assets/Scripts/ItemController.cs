ef813ed1e8d4fd55ab8d9da84d26dd208cf5e65f
MeriChon <meri@fina2.net>
2019-01-02 13:11:48 +0400

    commit before changing item list implementation (to grid layout)

---
+++ Homework3/Assets/Scripts/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemController : MonoBehaviour {
+    [SerializeField]
+    private Text _textField;
+
+    [SerializeField]
+    private RectTransform _contentTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Initialize(int index, Action callback)
+    {
+        _textField.text = "Item " + (index+1).ToString();
+        RectTransform itemTransform = transform.GetComponent<RectTransform>();
+        Debug.Log("Initial position" + itemTransform.position);
+        Debug.Log("Height is " + itemTransform.rect.height);
+
+        float height = itemTransform.rect.height;
+        Vector3 previousPosition = itemTransform.position;
+        itemTransform.position = previousPosition + new Vector3(0, -index * 1.3f);
+        Debug.Log("new position " + itemTransform.position);
+        //float y = (index - 1) * itemTransform.rect.height;
+        //transform.position = new Vector3(transform.position.x, y);
+    }
+}
---
+++ Homework3/Assets/Scripts/TaskListController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TaskListController : MonoBehaviour {
+    [SerializeField]
+    private Transform _itemPrefab;
+
+    private int _itemCounter;
+
+
+    // Use this for initialization
+    void Start () {
+        _itemCounter = 0;
+	}
+
+    public void AddItem() {
+        //Instantiate new List item prefab with index
+        Transform newItem = Instantiate(_itemPrefab);
+        newItem.SetParent(transform, false);
+        int index = _itemCounter;
+        newItem.GetComponent<ItemController>().Initialize(_itemCounter++, null);
+        //_itemCounter++;
+    }
+
+    public void OnDestroyItem(GameObject item)
+    {
+        Destroy(item);
+    }
+}
