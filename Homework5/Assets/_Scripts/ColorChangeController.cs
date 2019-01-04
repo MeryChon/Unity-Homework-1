93243e2612120fabc2f80d995bed4996336772eb
MeriChon <meri@fina2.net>
2019-01-04 21:23:28 +0400

    Gradient working

--- Homework5/Assets/_Scripts/ColorChangeController.cs
+++ Homework5/Assets/_Scripts/ColorChangeController.cs
@@ -3,60 +3,32 @@ using UnityEngine.UI;
 
 public class ColorChangeController : MonoBehaviour {
     [SerializeField]
-    private Image _cube;
+    private GameObject _cube;
     [SerializeField]
     private Color _color;
     [SerializeField]
     private bool _isUpperColor;
-    //[SerializeField]
-    private Color _initialColor;
-    [SerializeField]
-    private string _colorSymbol;
 
-    private float _initialSliderValue;
-
-    private float _previousSliderValue = 0;
-    private float _colorInterpolationT = 1;
+    private Color _initialColor = Color.black;
+    private float _previousSliderValue;
 
     void Start () {
         Slider slider = gameObject.GetComponent<Slider>();
-        _initialColor = _cube.color;
-        if (_colorSymbol == "r")
-        {
-            Debug.Log("Adding red " + _initialColor.r);
-            slider.value = _initialColor.r;
-            //UpdateColor(_initialColor.r);
-        }
-        else if (_colorSymbol == "g")
-        {
-            Debug.Log("Adding green " + _initialColor.g);
-            slider.value = _initialColor.g;
-        }
-        else if (_colorSymbol == "b")
-        {
-            Debug.Log("Adding blue " + _initialColor.b);
-            slider.value = _initialColor.b;
-        }
         _previousSliderValue = slider.value;
-        _colorInterpolationT = 0.5f;
 	}
 
     public void UpdateColor(float sliderValue)
     {
-        _initialColor = _cube.color;
-        Color targetColor = _initialColor + _color * (sliderValue - _previousSliderValue);
-        Color newColor = Color.Lerp(_initialColor, targetColor, _colorInterpolationT);
-        Debug.Log("Color of sphere was " + _cube.color);
-        _cube.color = newColor;
-        Debug.Log("New color of sphere is " + _cube.color);
-        /*if(_isUpperColor)
+        Material cubeMaterial = _cube.GetComponent<Renderer>().material;
+        string colorName = "_Color2";
+        if (_isUpperColor)
         {
-            _rectangle.GetComponent<Renderer>().material.SetColor("_UpperColor", newColor);
-        } else
-        {
-            _rectangle.GetComponent<Renderer>().material.SetColor("_BottomColor", newColor);
-        } */
+            colorName = "_Color1";
+        }
+        _initialColor = cubeMaterial.GetColor(colorName);
 
+        Color targetColor = _initialColor + _color * (sliderValue - _previousSliderValue);
+        cubeMaterial.SetColor(colorName, targetColor);
         _previousSliderValue = sliderValue;
     }
 }
